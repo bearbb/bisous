28d1aaeda87e60f7c6877fce01542afe84bb8c7d
bearbb <nguyen.b2uty@gmail.com>
2021-06-14 10:16:23 +0000

    Finished fixing thing related to create and deleted stuff on post

--- routes/comment.js
+++ routes/comment.js
@@ -75,7 +75,11 @@ commentRouter
           );
           //Delete commentId on comments arr on post data
           postDoc.comments.splice(commentIndex, 1);
-          postDoc = await comment.save();
+		let tc = postDoc.commentCount;
+		console.log("tc = ", tc);
+		postDoc.commentCount = tc - 1;
+		console.log("cm count", postDoc.commentCount);
+          postDoc = await postDoc.save();
           res
             .status(200)
             .json({ success: true, message: "Delete successfully" });
