055675cae4cf24ff2eca377d830cf0b9630e5b58
bearbb <nguyen.b2uty@gmail.com>
2021-08-13 15:26:09 +0700

    add some socket listener

--- socketServer/socketServer.js
+++ socketServer/socketServer.js
@@ -16,6 +16,27 @@ io.on("connection", (socket) => {
   console.log("connected");
   //init socket auth as false
   socket.auth = false;
+  socket.on("authenticateFromClient", ({ uid }) => {
+    socket.userId = uid;
+    socket.join(uid.toString());
+    console.log("Socket joined room ", uid);
+    socket.auth = true;
+  });
+
+  socket.on("privateMessage", ({ sender, receiver, message }) => {
+    socket
+      .to(receiver.userId.toString())
+      .emit("incomingMessage", { sender, receiver, message });
+    console.log("message forwarded");
+  });
+
+  //time out if client not send
+  setTimeout(() => {
+    if (!socket.auth) {
+      console.log("disconnect socket", socket.id);
+      socket.disconnect("unauthorized");
+    }
+  }, 1000);
 });
 
 module.exports = io;
